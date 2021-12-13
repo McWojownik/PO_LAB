931cbe0d3f8aa73d66e941aaa8e3851b3173a1f2
McWojownik <49338133+McWojownik@users.noreply.github.com>
2021-12-13 20:03:47 +0100

    LAB_8_FINISHED

---
+++ src/main/java/agh/ics/oop/gui/GridVisualizer.java
@@ -0,0 +1,67 @@
+package agh.ics.oop.gui;
+
+import agh.ics.oop.*;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.VBox;
+
+import java.io.FileNotFoundException;
+
+public class GridVisualizer {
+  private final IWorldMap map;
+  private final GridPane grid;
+
+  public GridVisualizer(IWorldMap map, GridPane grid) {
+    this.map = map;
+    this.grid = grid;
+  }
+
+  public void draw(Vector2d leftBottom, Vector2d rightTop) throws FileNotFoundException {
+    this.drawHeader(leftBottom, rightTop);
+    for (int i = rightTop.y; i >= leftBottom.y; i--) {
+      this.grid.getRowConstraints().add(new RowConstraints(40));
+      Label label = new Label(String.valueOf(i));
+      this.grid.add(label, 0, rightTop.y - i + 1);
+      GridPane.setHalignment(label, HPos.CENTER);
+      for (int j = leftBottom.x; j <= rightTop.x + 1; j++) {
+        if (j <= rightTop.x) {
+          VBox box = new VBox();
+          IMapElement obj = (IMapElement) this.getObject(new Vector2d(j, i));
+          if (obj != null) {
+            GuiElementBox el = new GuiElementBox(obj.getImageSource());
+            box.getChildren().add(el.getImage());
+            if (obj instanceof Animal) {
+              box.getChildren().add(el.addDescription("Z " + obj.getPosition().toString()));
+            } else {
+              box.getChildren().add(el.addDescription("Trawa"));
+            }
+          }
+          box.setAlignment(Pos.CENTER);
+          this.grid.add(box, j - leftBottom.x + 1, rightTop.y - i + 1);
+        }
+      }
+    }
+  }
+
+  private void drawHeader(Vector2d lowerLeft, Vector2d upperRight) {
+    Label label = new Label(" y\\x ");
+    this.grid.getRowConstraints().add(new RowConstraints(40));
+    this.grid.getColumnConstraints().add(new ColumnConstraints(40));
+    this.grid.add(label, 0, 0);
+    GridPane.setHalignment(label, HPos.CENTER);
+    for (int j = lowerLeft.x; j < upperRight.x + 1; j++) {
+      label = new Label(String.valueOf(j));
+      this.grid.add(label, j - lowerLeft.x + 1, 0);
+      this.grid.getColumnConstraints().add(new ColumnConstraints(40));
+      GridPane.setHalignment(label, HPos.CENTER);
+    }
+  }
+
+  private Object getObject(Vector2d currentPosition) {
+    return this.map.objectAt(currentPosition);
+  }
+}
---
+++ src/main/java/agh/ics/oop/gui/GuiElementBox.java
@@ -0,0 +1,28 @@
+package agh.ics.oop.gui;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class GuiElementBox {
+  private Image image;
+  private ImageView imageView;
+
+  public GuiElementBox(String pictureName) throws FileNotFoundException {
+    this.image = new Image(new FileInputStream("src/main/resources/" + pictureName));
+    this.imageView = new ImageView(this.image);
+    imageView.setFitWidth(20);
+    imageView.setFitHeight(20);
+  }
+
+  public Label addDescription(String text) {
+    return new Label(text);
+  }
+
+  public ImageView getImage() {
+    return this.imageView;
+  }
+}
